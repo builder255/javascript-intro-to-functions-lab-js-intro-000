be98a7583aa578229a5e3d99ca38547285a35381
Rich D <musiclike@hotmail.com>
2021-01-19 21:14:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-shout(string) {
-  return string.toUpperCase()
-}
+describe('shout(string)', function() {
+  it('receives one argument and returns it in all caps', function() {
+    expect(shout('hello')).toEqual('HELLO') }
